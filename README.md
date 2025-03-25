95344f3fee90dd8b90ae5685fa74f00d4a9c4f2b
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-03-25 04:43:55 +0000

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# if-else-lessons" 
+IF you want to follow along
+    https://crispy-space-couscous-975wqxxq9qwr276v9.github.dev/
+ELSE
+    reconsider, it is great practice
--- examples/LessonOne.java
+++ examples/LessonOne.java
@@ -1,5 +1,12 @@
 public class LessonOne {
     public static void main(String[] args) {
-        
+        int coins = 50;
+
+        if (coins >= 30) {
+            coins -= 30;
+            System.out.println("Congrats! You bought the battle-axe!");
+        } else {
+            System.out.println("I'm sorry, you can't afford the battle-axe.");
+        }
     }
 }
