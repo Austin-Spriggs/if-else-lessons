5aff7c5d00056caf06efeb9818d63d54bf045958
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-03-25 04:14:34 +0000

    initial setup

---
+++ exercises/ExerciseOne.java
@@ -0,0 +1,14 @@
+/*
+ * Write an if-else statement for a weather app that suggests how the user
+ * should dress or accessorize given different weather conditions
+ */
+public class ExerciseOne {
+    // heres some starter code to get the ideas flowing
+    public static void main(String[] args) {
+        String weather;
+        
+        if (???) {
+            System.out.println("Make sure to bring an umbrella today.");
+        }
+    }
+}
---
+++ runExamples.sh
@@ -0,0 +1,1 @@
+java -cp examples $1
